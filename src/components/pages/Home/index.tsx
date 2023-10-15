61e94b8c5159f2100229135bc727c62cd5112173
juicissKa <oleg.kurasov@gmail.com>
2023-10-15 13:37:58 +0300

    main page changes

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -3,29 +3,7 @@ import CategoryCard from "./CategoryCard";
 import styles from "./Home.module.scss";
 
 const Home: React.FC = () => {
-  return (
-    <div className={`container ${styles.root}`}>
-      <div className={styles.categories__big}>
-        <CategoryCard
-          title={"Рамки"}
-          imgurl={"/img/ramka.jpg"}
-          link={"catalog/"}
-        />
-        <CategoryCard
-          title={"Холсты"}
-          imgurl={"/img/holst.jpg"}
-          link={"canvas/"}
-        />
-        <CategoryCard title={"Картины"} imgurl={"/img/kartina.jpg"} />
-      </div>
-      <div className={styles.categories__small}>
-        <CategoryCard title={"Подарки"} imgurl={"/img/podarok.jpg"} />
-        <CategoryCard title={"Сувениры"} imgurl={"/img/souvenir.jpg"} />
-        <CategoryCard title={"Украшения"} imgurl={"/img/jewerly.jpg"} />
-        <CategoryCard title={"Другое"} imgurl={"/img/other.jpg"} />
-      </div>
-    </div>
-  );
+  return <div className={`container ${styles.root}`}></div>;
 };
 
 export default Home;

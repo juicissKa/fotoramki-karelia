e3767177ad99ba8891370b964851b325e8ba3f0f
juicissKa <oleg.kurasov@gmail.com>
2023-10-17 20:04:19 +0300

    main update

--- src/components/pages/Catalog/ProductBlock/index.tsx
+++ src/components/pages/Catalog/ProductBlock/index.tsx
@@ -15,7 +15,12 @@ const ProductBlock: React.FC<Product> = ({
   return (
     <div className={styles.root}>
       <Link to={String(id)}>
-        <img src={images ? images[0] : "/img/ramka.jpg"} alt="" />
+        <img
+          src={images ? images[0] : "/img/ramka.jpg"}
+          alt=""
+          width="296px"
+          height="296px"
+        />
       </Link>
 
       <div className={styles.info}>
